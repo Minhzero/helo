81913160ea0e700b9140f4ee1b83a938ab599b00
Minhzero <minhcongtu12321@gmail.com>
2023-10-30 15:59:13 +0700

    Initial commit

---
+++ app/src/main/java/minhnqph38692/fpoly/helo/MainActivity.java
@@ -0,0 +1,14 @@
+package minhnqph38692.fpoly.helo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
